3b65240ccbd5fa88f1abcb5b7f15cf06fd2f0d0d
alejandro <alejandrop@bluetrailsoft.com>
2024-03-08 09:39:14 -0600

    [lsp]: language servers updated

---
+++ init.lua
@@ -0,0 +1,3 @@
+require('alejandro.remap')
+require('alejandro.set')
+
---
+++ lua/alejandro/packer.lua
@@ -0,0 +1,55 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- schema
+  use ({ 
+    "rose-pine/neovim",
+    as = "rose-pine",
+    config = function ()
+	    vim.cmd('colorscheme rose-pine')
+	end
+    })
+
+  -- treesitter
+  use ({
+    'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}
+  })
+
+  -- lsp
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      -- lenguage sever 
+      {"williamboman/mason.nvim"},
+      {"williamboman/mason-lspconfig.nvim"},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  }
+
+  -- install without yarn or npm
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+ -- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+end)
